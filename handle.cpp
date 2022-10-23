5a94c8dcfffca488177ba1420de86510db2a41ec
qc0ld <then7w@yandex.ru>
2022-10-23 18:17:44 +0300

    тесты запускаются в мейне

--- handle.cpp
+++ handle.cpp
@@ -21,7 +21,7 @@ int read_menu_int(const char *name) {
     int number;
     std::cout << "Enter " << name << ": ";
     cin >> number;
-    while (cin.fail() || (number < 0 || number > 10)) {
+    while (cin.fail() || (number < 0 || number > 11)) {
         cin.clear();
         cin.ignore(1024, '\n');
         cout << "Enter correct number: " << endl;
